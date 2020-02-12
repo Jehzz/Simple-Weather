e779e03629f257f8d1e12732b15479bb81bb0d57
Jess <jesseboi@protonmail.com>
2020-02-12 12:01:04 -0500

    Added apikey and url format

--- app/src/main/java/com/example/weatherapp/model/RetrofitEndpoint.kt
+++ app/src/main/java/com/example/weatherapp/model/RetrofitEndpoint.kt
@@ -6,6 +6,8 @@ import retrofit2.http.GET
 
 interface RetrofitEndpoint {
 
+
+
     //Previous example
     //https://api.androidhive.info/json/movies.json
     //@GET("movies.json")
@@ -21,6 +23,10 @@ interface RetrofitEndpoint {
     //@GET("????")  figure out how to set this value to zip code
     //fun getWeather(): Observable<List<PokoWeatherData>>
 
+    //url = http://api.openweathermap.org/data/2.5/forecast?q=
+    //city = CITY
+    //apikey = &appid=ca3efb1692ca390683b47b41ade98581
+    //final url = url+city+apikey
 
     @GET("????")
     fun getWeather(): Call<List<PokoWeatherData>>
