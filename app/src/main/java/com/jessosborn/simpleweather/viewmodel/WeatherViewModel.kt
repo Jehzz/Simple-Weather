e7f60d1b3da599a0b6852002c5f2c26fd164f036
Jess <jesseboi@protonmail.com>
2022-05-12 23:08:47 -0500

    Minor refactoring, LiveData by lazy

--- app/src/main/java/com/jessosborn/simpleweather/viewmodel/WeatherViewModel.kt
+++ app/src/main/java/com/jessosborn/simpleweather/viewmodel/WeatherViewModel.kt
@@ -1,21 +1,18 @@
 package com.jessosborn.simpleweather.viewmodel
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import com.jessosborn.simpleweather.model.repository.CurrentWeatherData
-import com.jessosborn.simpleweather.model.repository.ForecastWeatherData
 import com.jessosborn.simpleweather.model.repository.WeatherRepository
 import com.jessosborn.simpleweather.utils.getCountryFromZip
 import javax.inject.Inject
 
 class WeatherViewModel @Inject constructor(
-    private val weatherRepo: WeatherRepository,
+    private val weatherRepo: WeatherRepository
 ) : ViewModel() {
 
-    val currentWeatherDataSet: LiveData<CurrentWeatherData> = weatherRepo.currentWeatherData
-    val forecastWeatherDataSet: LiveData<ForecastWeatherData> = weatherRepo.forecastWeatherData
-    val isNetworkLoading: LiveData<Boolean> = weatherRepo.isNetworkLoading
-    val networkError: LiveData<String> = weatherRepo.errorMessage
+    val currentWeatherDataSet by lazy { weatherRepo.currentWeatherData }
+    val forecastWeatherDataSet by lazy { weatherRepo.forecastWeatherData }
+    val isNetworkLoading by lazy { weatherRepo.isNetworkLoading }
+    val networkError by lazy { weatherRepo.errorMessage }
 
     fun fetchWeatherFromApi(zip: String, units: String, key: String) {
         weatherRepo.fetchWeatherFromApi(zip, getCountryFromZip(zip), units, key)
