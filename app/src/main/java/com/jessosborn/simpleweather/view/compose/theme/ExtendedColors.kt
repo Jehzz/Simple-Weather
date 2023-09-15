5cb4f72ab7629663ef3d8f991eeb7dfcb7d776c0
Jehzz <jesseboi@protonmail.com>
2023-09-14 20:19:43 -0500

    Material You support

---
+++ app/src/main/java/com/jessosborn/simpleweather/view/compose/theme/ExtendedColors.kt
@@ -0,0 +1,25 @@
+package com.jessosborn.simpleweather.view.compose.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.graphics.Color
+
+@Immutable
+data class ExtendedColors(
+	val hot: Color,
+	val cold: Color,
+)
+
+val LocalExtendedColors = staticCompositionLocalOf {
+	ExtendedColors(
+		hot = Color.Unspecified,
+		cold = Color.Unspecified
+	)
+}
+
+object ExtendedTheme {
+	val colors: ExtendedColors
+		@Composable
+		get() = LocalExtendedColors.current
+}
