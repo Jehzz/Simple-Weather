fce531061a92af7fd03de8506dce543f43891cf6
Jess <jesseboi@protonmail.com>
2025-03-14 22:01:27 -0600

    Colorful borders

--- app/src/main/java/com/jessosborn/simpleweather/view/compose/components/ForecastLayout.kt
+++ app/src/main/java/com/jessosborn/simpleweather/view/compose/components/ForecastLayout.kt
@@ -1,28 +1,38 @@
 package com.jessosborn.simpleweather.view.compose.components
 
 import android.text.format.DateUtils
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
 import com.jessosborn.simpleweather.R
+import com.jessosborn.simpleweather.domain.Units
 import com.jessosborn.simpleweather.domain.remote.responses.ForecastWeather
 import com.jessosborn.simpleweather.domain.remote.responses.Main
 import com.jessosborn.simpleweather.domain.remote.responses.WeatherData
 import com.jessosborn.simpleweather.domain.remote.responses.WeatherSnapshot
 import com.jessosborn.simpleweather.utils.CombinedPreviews
+import com.jessosborn.simpleweather.utils.DataStoreUtil
+import com.jessosborn.simpleweather.view.compose.theme.ExtendedTheme
 import com.jessosborn.simpleweather.view.compose.theme.SimpleWeatherTheme
 import java.text.SimpleDateFormat
 import java.util.Calendar
@@ -38,7 +48,7 @@ fun ForecastLayout(
 	val dayAftersWeather = partitionedWeather.second.drop(8).take(8)
 
 	Column(
-		modifier = Modifier.padding(vertical = 8.dp),
+		modifier = Modifier.padding(all = 8.dp),
 		verticalArrangement = Arrangement.spacedBy(12.dp)
 	) {
 		if (todaysWeather.isNotEmpty()) {
@@ -68,7 +78,17 @@ private fun ForecastRow(
 	onSnapshotSelected: (WeatherSnapshot) -> Unit
 ) {
 	LazyRow(
-		modifier = Modifier.fillMaxWidth(),
+		modifier = Modifier
+			.fillMaxWidth()
+			.border(
+				width = 1.dp,
+				brush = Brush.horizontalGradient(
+					colors = forecastBorderColors(
+						temps = Triple(weather.firstOrNull(), weather.getOrNull(weather.size / 2), weather.lastOrNull())
+					)
+				),
+				shape = RoundedCornerShape(10.dp)
+			),
 		horizontalArrangement = Arrangement.SpaceEvenly
 	) {
 		items(weather) { item ->
@@ -77,6 +97,36 @@ private fun ForecastRow(
 				item = item,
 				onClick = { onSnapshotSelected(it) }
 			)
+		}
+	}
+}
+
+
+@Composable
+private fun forecastBorderColors(
+	temps: Triple<WeatherSnapshot?, WeatherSnapshot?, WeatherSnapshot?>
+): List<Color> {
+	val startingColor = getColorFromTemp(temps.first?.main?.temp ?: 0f)
+	val middleColor = getColorFromTemp(temps.second?.main?.temp ?: 0f)
+	val endingColor = getColorFromTemp(temps.third?.main?.temp ?: 0f)
+	return listOf(startingColor, middleColor, endingColor)
+}
+
+@Composable
+private fun getColorFromTemp(temp: Float): Color {
+	val units by DataStoreUtil.getUnits(LocalContext.current).collectAsState(initial = Units.Imperial)
+
+	return when (units) {
+		Units.Metric -> if (temp < 21.1) {
+			ExtendedTheme.colors.cold
+		} else {
+			ExtendedTheme.colors.hot
+		}
+
+		Units.Imperial -> if (temp < 70.0) {
+			ExtendedTheme.colors.cold
+		} else {
+			ExtendedTheme.colors.hot
 		}
 	}
 }
